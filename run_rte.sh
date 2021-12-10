c78db0a4246a181593bc9a5ae8200c637588af30
leeyy2020 <liyueyang01@baidu.com>
2021-12-10 06:55:27 +0000

    add sh

---
+++ run_rte.sh
@@ -0,0 +1,29 @@
+for seed in 13 21 42 87 100
+do
+   str1='{
+      "benchmark":"super_glue",
+      "classifier_init":{
+         "entailment":" yes",
+         "not_entailment":" instead"
+      },
+      "dataset":"few_rte",
+      "grad_norm":1,
+      "lr":0.001,
+      "max_batch_size":4,
+      "max_tokens_sq":131072,
+      "num_epochs":30,
+      "num_gradient_accumulation_steps":2,
+      "prompt_better_init":"[PAD]",
+      "prompts":[-10,-11,[-14,"\""],null,[-15,"\""],[-16,"?"],"<mask>",[-20,","],null,[-29,"!"],-30,-31],
+      "seed":'
+   str2=',"str_cut_frac":0.06,
+      "transformer_model":"albert-xxlarge-v2",
+      "validation_metric":"+training_val_metric"
+   }'
+   DATASET=few_rte
+   echo $str1$seed$str2
+   export HPARAMS=$str1$seed$str2
+   CUDA_VISIBLE_DEVICES=1 ./python3.7.0/bin/python3.7 -m allennlp train \
+   -s .aim/few_rte_bs4/t-${DATASET}_seed_${seed} configs/warp.jsonnet
+   rm -f .aim/few_rte_bs4/t-${DATASET}_seed_${seed}/model*
+done
